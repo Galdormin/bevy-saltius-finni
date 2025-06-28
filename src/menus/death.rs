6052f51761e3c90a57a9540116bf83f1d4a839cd
Galdormin <theoplenet@gmail.com>
2025-06-28 19:14:40 +0200

    feat: Add GeneDatabase from RON + Simple gene selection at death

--- src/menus/death.rs
+++ src/menus/death.rs
@@ -1,23 +1,93 @@
 //! Death menu to choose the genes
 
-use bevy::prelude::*;
+use bevy::ui::Val::*;
+use bevy::{ecs::spawn::SpawnIter, prelude::*};
 
-use crate::{event::RespawnEvent, menus::Menu, ui::widget};
+use crate::player::genes::Gene;
+use crate::{event::RespawnEvent, menus::Menu, player::genes::PlayerGenes, ui::widget};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Menu::Death), spawn_death_menu);
 }
 
-fn spawn_death_menu(mut commands: Commands) {
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct GeneButton(usize);
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct InactiveGeneContainer;
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct ActiveGeneContainer;
+
+fn spawn_death_menu(mut commands: Commands, player_genes: Res<PlayerGenes>) {
     commands.spawn((
         widget::ui_root("Death Menu"),
         GlobalZIndex(2),
         StateScoped(Menu::Death),
         children![
             widget::header("You have died!"),
+            gene_choice(&player_genes),
             widget::button("Respawn", respawn),
         ],
     ));
+}
+
+fn gene_choice(player_genes: &PlayerGenes) -> impl Bundle {
+    let inactive_genes: Vec<Gene> = player_genes
+        .inactive_genes()
+        .iter()
+        .map(|g| g.to_owned().clone())
+        .collect();
+
+    let active_genes: Vec<Gene> = player_genes
+        .active_genes()
+        .iter()
+        .map(|g| g.to_owned().clone())
+        .collect();
+
+    (
+        Name::new("Gene menu"),
+        Node {
+            display: Display::Grid,
+            row_gap: Px(10.0),
+            column_gap: Px(30.0),
+            grid_template_columns: RepeatedGridTrack::px(2, 400.0),
+            ..default()
+        },
+        children![
+            (
+                Name::new("Inactive genes"),
+                Node {
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                InactiveGeneContainer,
+                Children::spawn(SpawnIter(
+                    inactive_genes
+                        .into_iter()
+                        .map(|gene| gene_inactive_button(gene))
+                ))
+            ),
+            (
+                Name::new("Active genes"),
+                Node {
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ActiveGeneContainer,
+                Children::spawn(SpawnIter(
+                    active_genes
+                        .into_iter()
+                        .map(|gene| gene_active_button(gene))
+                ))
+            )
+        ],
+    )
 }
 
 fn respawn(
@@ -27,4 +97,77 @@ fn respawn(
 ) {
     next_menu.set(Menu::None);
     respawn_event.write(RespawnEvent);
+}
+
+fn gene_inactive_button(gene: Gene) -> impl Bundle {
+    (
+        GeneButton(gene.id),
+        widget::button(gene.name.clone(), add_gene(gene)),
+    )
+}
+
+fn gene_active_button(gene: Gene) -> impl Bundle {
+    (
+        GeneButton(gene.id),
+        widget::button(gene.name.clone(), remove_gene(gene)),
+    )
+}
+
+fn add_gene(
+    gene: Gene,
+) -> impl Fn(
+    Trigger<Pointer<Click>>,
+    Commands,
+    ResMut<PlayerGenes>,
+    Single<Entity, With<ActiveGeneContainer>>,
+    Query<(Entity, &GeneButton)>,
+) {
+    move |_, mut commands, mut player_genes, active_container, gene_buttons| {
+        info!("Add Gene: {}", gene.name);
+
+        // Find the GeneButton Entity
+        let Some((gene_entity, _)) = gene_buttons.iter().find(|(_, button)| button.0 == gene.id)
+        else {
+            return;
+        };
+
+        player_genes.add_active_gene(gene.id);
+
+        // Delete the button
+        commands.entity(gene_entity).despawn();
+
+        // Add a new GeneButton to the InactiveContainer
+        commands.spawn((gene_active_button(gene.clone()), ChildOf(*active_container)));
+    }
+}
+
+fn remove_gene(
+    gene: Gene,
+) -> impl Fn(
+    Trigger<Pointer<Click>>,
+    Commands,
+    ResMut<PlayerGenes>,
+    Single<Entity, With<InactiveGeneContainer>>,
+    Query<(Entity, &GeneButton)>,
+) {
+    move |_, mut commands, mut player_genes, inactive_container, gene_buttons| {
+        info!("Remove Gene: {}", gene.name);
+
+        // Find the GeneButton Entity
+        let Some((gene_entity, _)) = gene_buttons.iter().find(|(_, button)| button.0 == gene.id)
+        else {
+            return;
+        };
+
+        player_genes.remove_active_gene(gene.id);
+
+        // Delete the button
+        commands.entity(gene_entity).despawn();
+
+        // Add a new GeneButton to the InactiveContainer
+        commands.spawn((
+            gene_inactive_button(gene.clone()),
+            ChildOf(*inactive_container),
+        ));
+    }
 }
--- src/player/genes.rs
+++ src/player/genes.rs
@@ -1,31 +1,38 @@
 //! Code for the genes of the player
 
+use bevy::platform::collections::HashMap;
 use bevy::{math::FloatPow, prelude::*};
+
+use bevy_common_assets::ron::RonAssetPlugin;
+use serde::Deserialize;
 
 use crate::player::movement::{JumpAmount, JumpImpulse, MovementSpeed};
 use crate::player::physics::{CharacterController, GravityController};
+use crate::screens::Screen;
 
 pub(super) fn plugin(app: &mut App) {
+    app.add_plugins(RonAssetPlugin::<GeneDatabase>::new(&["genes.ron"]));
+
+    app.add_systems(Startup, load_gene_database);
     app.add_systems(Update, player_genes_changed);
+    app.add_systems(OnEnter(Screen::Title), load_default_gene); // Change
 
     app.init_resource::<PlayerGenes>();
-    app.insert_resource(PlayerGenes(vec![Gene {
-        name: "Rabbit heritage".into(),
-        stats: vec![
-            GeneStat::JumpAmount(5),
-            GeneStat::JumpHeight(5.0),
-            GeneStat::MovementSpeed(8.0),
-        ],
-    }]));
 }
 
+#[derive(Asset, Clone, Debug, Deserialize, Reflect)]
+pub struct GeneDatabase(Vec<Gene>);
+
+#[derive(Resource)]
+pub struct GeneDatabaseHandle(Handle<GeneDatabase>);
+
 /// Enum for the different Gene stats
-#[derive(Clone, Debug, Reflect)]
+#[derive(Clone, Debug, Deserialize, Reflect)]
 pub enum GeneStat {
     JumpAmount(i32),    // Number of jumps before death
     JumpHeight(f32),    // Jump height in blocks (8 pixels)
     MovementSpeed(f32), // Movement speed in blocks/sec
-    PowerUp,            // Power Up
+    PowerUp(String),    // Power Up
 }
 
 impl Default for GeneStat {
@@ -35,9 +42,10 @@ impl Default for GeneStat {
 }
 
 /// Struct that describe a gene
-#[derive(Clone, Debug, Default, Reflect)]
+#[derive(Clone, Debug, Default, Deserialize, Reflect)]
 pub struct Gene {
-    name: String,
+    pub id: usize,
+    pub name: String,
     stats: Vec<GeneStat>,
 }
 
@@ -85,19 +93,71 @@ impl Gene {
 /// Resource with all the genes of the character
 #[derive(Resource, Reflect, Debug, Default)]
 #[reflect(Resource)]
-pub struct PlayerGenes(Vec<Gene>);
+pub struct PlayerGenes {
+    base: Gene,
+    genes: Vec<usize>,
+    known: HashMap<usize, Gene>,
+}
 
 impl PlayerGenes {
+    pub fn add_active_gene(&mut self, gene_id: usize) {
+        self.genes.push(gene_id);
+    }
+
+    pub fn remove_active_gene(&mut self, gene_id: usize) {
+        if let Some(index) = self.genes.iter().position(|gid| *gid == gene_id) {
+            self.genes.remove(index);
+        }
+    }
+
+    /// Return active genes
+    pub fn active_genes(&self) -> Vec<&Gene> {
+        self.genes
+            .iter()
+            .filter_map(|id| self.known.get(id))
+            .collect::<Vec<&Gene>>()
+    }
+
+    /// Return known but inactive genes
+    pub fn inactive_genes(&self) -> Vec<&Gene> {
+        self.known
+            .iter()
+            .filter_map(|(id, gene)| {
+                if self.genes.contains(id) {
+                    None
+                } else {
+                    Some(gene)
+                }
+            })
+            .collect::<Vec<&Gene>>()
+    }
+
+    /// Return active genes + base gene
+    pub fn all_active_genes(&self) -> Vec<&Gene> {
+        let mut genes = self.active_genes();
+        genes.push(&self.base);
+        genes
+    }
+
     fn total_jump_amount(&self) -> i32 {
-        self.0.iter().map(|gene| gene.get_jump_amount()).sum()
+        self.all_active_genes()
+            .iter()
+            .map(|gene| gene.get_jump_amount())
+            .sum()
     }
 
     fn total_jump_height(&self) -> f32 {
-        self.0.iter().map(|gene| gene.get_jump_height()).sum()
+        self.all_active_genes()
+            .iter()
+            .map(|gene| gene.get_jump_height())
+            .sum()
     }
 
     fn total_movement_speed(&self) -> f32 {
-        self.0.iter().map(|gene| gene.get_movement_speed()).sum()
+        self.all_active_genes()
+            .iter()
+            .map(|gene| gene.get_movement_speed())
+            .sum()
     }
 }
 
@@ -131,4 +191,23 @@ fn player_genes_changed(
 
     controller_gravity.jump_gravity = (2.0 * jump_height * 8.0) / jump_time.squared();
     jump_impulse.0 = controller_gravity.jump_gravity * jump_time;
+}
+
+fn load_gene_database(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let gene_database = GeneDatabaseHandle(asset_server.load("resources/genes.ron"));
+    commands.insert_resource(gene_database);
+}
+
+fn load_default_gene(
+    mut commands: Commands,
+    gene_db: Res<GeneDatabaseHandle>,
+    mut gene_databases: ResMut<Assets<GeneDatabase>>,
+) {
+    if let Some(gene_db) = gene_databases.remove(gene_db.0.id()) {
+        commands.insert_resource(PlayerGenes {
+            base: gene_db.0[0].clone(),
+            genes: vec![1],
+            known: HashMap::from([(1, gene_db.0[1].clone()), (2, gene_db.0[2].clone())]),
+        });
+    }
 }
